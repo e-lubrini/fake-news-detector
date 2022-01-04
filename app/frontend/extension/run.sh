a3c19f8626a2e431f104844aeb2ca2ffe3d0cb66
e-lubrini <lubrinie@gmail.com>
2022-01-04 14:14:02 +0100

    separated website from extension

---
+++ app/frontend/extension/run.sh
@@ -0,0 +1,1 @@
+source ../../venv/bin/activate && export FLASK_APP=app.py && export_FLASK_ENV=development && flask run
--- app/frontend/website/app.py
+++ app/frontend/website/app.py
@@ -15,20 +15,17 @@ Bootstrap(app)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-print(os.getcwd())
+templates_path = ''
 
 @app.route('/') # converts the return value into an HTTP response to be displayed by an HTTP client
 def index():
     form = URLForm()
-    return render_template('website/index.html', form=form)
-@app.route('/')
-def popup():
-    return render_template('extensions/popup.html')
+    return render_template(templates_path+'index.html', form=form)
 
 @app.route('/', methods=['POST'])
 def handle_data():
     form = URLForm()
     url = request.form.get('url',0)
-    from .. backend import pipeline
+    from ...backend import pipeline
     results = pipeline.start(url)
-    return render_template('results.html', likelihood=results['likelihood'])
+    return render_template(templates_path+'results.html', likelihood=results['likelihood'])
