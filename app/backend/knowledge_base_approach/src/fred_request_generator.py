e029449759a175376f9565f5c3c20f26ed81d311
e-lubrini <lubrinie@gmail.com>
2022-01-09 22:27:35 +0100

    basic script generating request url from txt

---
+++ app/backend/knowledge_base_approach/src/fred_request_generator.py
@@ -0,0 +1,25 @@
+import subprocess
+import urllib.parse
+
+txt = '''
+This is a sample sentence.
+'''
+
+## TODO split sentences and process individually
+
+try:
+    with open('token.txt') as f:
+        token = f.readline()
+except:
+    print('No token found!')
+
+outputfile_name = 'try.ttl'
+
+encoded_txt = urllib.parse.quote(txt)
+print(encoded_txt)
+
+bashCmd = 'curl -X GET "http://wit.istc.cnr.it/stlab-tools/fred/?text=' + encoded_txt + '&semantic-subgraph=true" -H  "accept: text/turtle" -H  "Authorization: Bearer ' + token + '" >> ' + outputfile_name
+
+process = subprocess.Popen(bashCmd,
+                            shell=True,
+                            stdout=subprocess.PIPE)
